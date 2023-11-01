e97cff2c2d1da5482c0facf654ff41140df1b07b
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-11-01 21:00:19 +0100

    initial setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-background dark text-foreground-500' >
         <Main />
         <NextScript />
       </body>
