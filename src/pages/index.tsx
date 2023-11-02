d88cfd2a353cd2bf728792ad10347bd251102a37
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-11-02 14:00:47 +0100

    Delay time table + Copy/paste on click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,25 +25,25 @@ export default function Home() {
     }
   };
 
-  const handleCellClick = (event: React.MouseEvent<HTMLTableCellElement>) => {
-    const cellValue = event.currentTarget.textContent || "";
-    navigator.clipboard.writeText(cellValue);
+  const handleValueClick = (event: React.MouseEvent<HTMLSpanElement>) => {
+    const value = event.currentTarget.textContent || "";
+    navigator.clipboard.writeText(value);
 
     setShowNotification(true);
 
     setTimeout(() => setShowNotification(false), 1000);
   };
 
-  const cellProps = {
-    onClick: handleCellClick,
-    className: "cursor-copy max-w-fit",
+  const valueProps = {
+    onClick: handleValueClick,
+    className: "cursor-copy ",
   };
 
   return (
-    <div className="min-h-screen py-24 lg:py-32">
+    <div className=" py-24 lg:py-32 min-h-screen">
       {/* Copied notification */}
       {showNotification && (
-        <div className=" flex align-baseline backdrop-blur-xl border-1 border-default-500 text-default-500  z-30 rounded-xl p-2 absolute bottom-16 right-16 ">
+        <div className=" flex align-baseline backdrop-blur-xl border-1 border-default-500 text-default-500  z-30 rounded-xl p-2 fixed bottom-16 right-16 ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -82,19 +82,20 @@ export default function Home() {
           fullWidth={true}
           onChange={handleChange}
         />
- {/* Reverb Times Section */}
+        {/* Reverb Times Section */}
         <h2>Reverb Times</h2>
         <div className="max-w-4xl mx-auto">
           <Table
-            aria-label="Example static collection table"
+            aria-label="Reverb Times Table"
             classNames={{ th: " text-lg text-default-800", td: "text-base" }}
-          
+            selectionMode="single"
+            selectionBehavior="replace"
           >
             <TableHeader>
-              <TableColumn>Reverb Size</TableColumn>
-              <TableColumn>Pre-Delay</TableColumn>
-              <TableColumn>Decay Time</TableColumn>
-              <TableColumn>Total Reverb Time</TableColumn>
+              <TableColumn width={300}>Reverb Size</TableColumn>
+              <TableColumn width={200}>Pre-Delay (ms)</TableColumn>
+              <TableColumn width={200}>Decay Time (ms)</TableColumn>
+              <TableColumn width={200}>Total Reverb Time (ms)</TableColumn>
             </TableHeader>
             <TableBody>
               <TableRow key="1">
@@ -102,57 +103,80 @@ export default function Home() {
                   Tight Ambience{" "}
                   <span className="text-default-500">- (1/4 Note)</span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbTightAmbience.preDelay}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbTightAmbience.preDelay}
+                  </span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbTightAmbience.decayTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbTightAmbience.decayTime}
+                  </span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbTightAmbience.totalReverbTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbTightAmbience.totalReverbTime}
+                  </span>
                 </TableCell>
               </TableRow>
+
               <TableRow key="2">
                 <TableCell className="text-default-700  ">
                   Small Room{" "}
                   <span className="text-default-500">- (1/2 Note)</span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbSmallRoom.preDelay}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbSmallRoom.preDelay}
+                  </span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbSmallRoom.decayTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbSmallRoom.decayTime}
+                  </span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbSmallRoom.totalReverbTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbSmallRoom.totalReverbTime}
+                  </span>
                 </TableCell>
               </TableRow>
+
               <TableRow key="3">
                 <TableCell className="text-default-700  ">
                   Large Room <span className="text-default-500">- (1 Bar)</span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbLargeRoom.preDelay}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbLargeRoom.preDelay}
+                  </span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbLargeRoom.decayTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbLargeRoom.decayTime}
+                  </span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbLargeRoom.totalReverbTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbLargeRoom.totalReverbTime}
+                  </span>
                 </TableCell>
               </TableRow>
+
               <TableRow key="4">
                 <TableCell className="text-default-700  ">
                   Hall <span className="text-default-500">- (2 Bars)</span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbHall.preDelay}
+                <TableCell>
+                  <span {...valueProps}>{results.reverbHall.preDelay}</span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbHall.decayTime}
+                <TableCell>
+                  <span {...valueProps}>{results.reverbHall.decayTime}</span>
                 </TableCell>
-                <TableCell {...cellProps}>
-                  {results.reverbHall.totalReverbTime}
+                <TableCell>
+                  <span {...valueProps}>
+                    {results.reverbHall.totalReverbTime}
+                  </span>
                 </TableCell>
               </TableRow>
             </TableBody>
@@ -160,6 +184,189 @@ export default function Home() {
         </div>
         {/* Delay Times Section */}
         <h2>Delay Times & LFO frequencies</h2>
+
+        <div className="max-w-4xl mx-auto">
+          <Table
+            aria-label="Delay Times and LFO Frequencies table"
+            classNames={{ th: " text-lg text-default-800", td: "text-base" }}
+            selectionMode="single"
+            selectionBehavior="replace"
+          >
+            <TableHeader>
+              <TableColumn width={300}>Note Value</TableColumn>
+              <TableColumn width={200}>Notes</TableColumn>
+              <TableColumn width={200}>Dotted</TableColumn>
+              <TableColumn width={200}>Triplet</TableColumn>
+            </TableHeader>
+            <TableBody>
+              <TableRow key="1">
+                <TableCell className="text-default-700">
+                  1/1 <span className="text-default-500">- (1 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.wholeNote}</span> ms <br />
+                  <span {...valueProps}>{results.wholeNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedWholeNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.dottedWholeNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletWholeNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.tripletWholeNoteFreq}</span> Hz
+                </TableCell>
+              </TableRow>
+              <TableRow key="2">
+                <TableCell className="text-default-700">
+                  1/2 <span className="text-default-500">- (1/2 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.halfNote}</span> ms <br />
+                  <span {...valueProps}>{results.halfNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedHalfNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.dottedHalfNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletHalfNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.tripletHalfNoteFreq}</span> Hz
+                </TableCell>
+              </TableRow>
+              <TableRow key="3">
+                <TableCell className="text-default-700">
+                  1/4 <span className="text-default-500">- (1/4 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.quarterNote}</span> ms <br />
+                  <span {...valueProps}>{results.quarterNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedQuarterNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>
+                    {results.dottedQuarterNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletQuarterNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>
+                    {results.tripletQuarterNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+              </TableRow>
+              <TableRow key="4">
+                <TableCell className="text-default-700">
+                  1/8 <span className="text-default-500">- (1/8 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.eighthNote}</span> ms <br />
+                  <span {...valueProps}>{results.eighthNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedEighthNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.dottedEighthNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletEighthNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>
+                    {results.tripletEighthNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+              </TableRow>
+
+              <TableRow key="5">
+                <TableCell className="text-default-700">
+                  1/16 <span className="text-default-500">- (1/16 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.sixteenthNote}</span> ms <br />
+                  <span {...valueProps}>{results.sixteenthNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedSixteenthNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>
+                    {results.dottedSixteenthNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletSixteenthNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>
+                    {results.tripletSixteenthNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+              </TableRow>
+
+              <TableRow key="6">
+                <TableCell className="text-default-700">
+                  1/32 <span className="text-default-500">- (1/32 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.thirtySecondNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.thirtySecondNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedThirtySecondNote}</span>{" "}
+                  ms <br />
+                  <span {...valueProps}>
+                    {results.dottedThirtySecondNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletThirtySecondNote}</span>{" "}
+                  ms <br />
+                  <span {...valueProps}>
+                    {results.tripletThirtySecondNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+              </TableRow>
+
+              <TableRow key="7">
+                <TableCell className="text-default-700">
+                  1/64 <span className="text-default-500">- (1/64 Bar)</span>
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.sixtyFourthNote}</span> ms{" "}
+                  <br />
+                  <span {...valueProps}>{results.sixtyFourthNoteFreq}</span> Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.dottedSixtyFourthNote}</span>{" "}
+                  ms <br />
+                  <span {...valueProps}>
+                    {results.dottedSixtyFourthNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+                <TableCell>
+                  <span {...valueProps}>{results.tripletSixtyFourthNote}</span>{" "}
+                  ms <br />
+                  <span {...valueProps}>
+                    {results.tripletSixtyFourthNoteFreq}
+                  </span>{" "}
+                  Hz
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
       </div>
     </div>
   );
--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -7,7 +7,7 @@ export function calculateDelaysAndReverb(bpm: number) {
 
   function transformValues(obj: any) {
     for (let key in obj) {
-      if (typeof obj[key] === 'object') {
+      if (typeof obj[key] === "object") {
         obj[key] = transformValues(obj[key]);
       } else {
         obj[key] = convertToNumber(obj[key]);
@@ -16,10 +16,9 @@ export function calculateDelaysAndReverb(bpm: number) {
     return obj;
   }
 
-  const rawResults =  {
+  const rawResults = {
     // Regular Note Delays
     wholeNote: baseDelay * 4,
-    wholeNotefreq: 1000 / (baseDelay * 4),
     halfNote: baseDelay * 2,
     quarterNote: baseDelay,
     eighthNote: baseDelay / 2,
@@ -45,30 +44,61 @@ export function calculateDelaysAndReverb(bpm: number) {
     tripletQuarterNote: baseDelay * (2 / 3),
     tripletEighthNote: (baseDelay / 2) * (2 / 3),
     tripletSixteenthNote: (baseDelay / 4) * (2 / 3),
+    tripletThirtySecondNote: (baseDelay / 8) * (2 / 3),
+    tripletSixtyFourthNote: (baseDelay / 16) * (2 / 3),
+
+    // Regular Note Frequencies
+    wholeNoteFreq: 1000 / (baseDelay * 4),
+    halfNoteFreq: 1000 / (baseDelay * 2),
+    quarterNoteFreq: 1000 / baseDelay,
+    eighthNoteFreq: 1000 / (baseDelay / 2),
+    sixteenthNoteFreq: 1000 / (baseDelay / 4),
+    thirtySecondNoteFreq: 1000 / (baseDelay / 8),
+    sixtyFourthNoteFreq: 1000 / (baseDelay / 16),
+    oneHundredTwentyEighthNoteFreq: 1000 / (baseDelay / 32),
+    twoHundredFiftySixthNoteFreq: 1000 / (baseDelay / 64),
+    fiveHundredTwelfthNoteFreq: 1000 / (baseDelay / 128),
+
+    // Dotted Note Frequencies
+    dottedWholeNoteFreq: 1000 / (baseDelay * 4 * 1.5),
+    dottedHalfNoteFreq: 1000 / (baseDelay * 2 * 1.5),
+    dottedQuarterNoteFreq: 1000 / (baseDelay * 1.5),
+    dottedEighthNoteFreq: 1000 / ((baseDelay / 2) * 1.5),
+    dottedSixteenthNoteFreq: 1000 / ((baseDelay / 4) * 1.5),
+    dottedThirtySecondNoteFreq: 1000 / ((baseDelay / 8) * 1.5),
+    dottedSixtyFourthNoteFreq: 1000 / ((baseDelay / 16) * 1.5),
+
+    // Triplet Note Frequencies
+    tripletWholeNoteFreq: 1000 / (baseDelay * 4 * (2 / 3)),
+    tripletHalfNoteFreq: 1000 / (baseDelay * 2 * (2 / 3)),
+    tripletQuarterNoteFreq: 1000 / (baseDelay * (2 / 3)),
+    tripletEighthNoteFreq: 1000 / ((baseDelay / 2) * (2 / 3)),
+    tripletSixteenthNoteFreq: 1000 / ((baseDelay / 4) * (2 / 3)),
+    tripletThirtySecondNoteFreq: 1000 / ((baseDelay / 8) * (2 / 3)),
+    tripletSixtyFourthNoteFreq: 1000 / ((baseDelay / 16) * (2 / 3)),
 
     // Reverb Times
     reverbHall: {
       preDelay: baseDelay / 8,
-      decayTime: (baseDelay * 4 * 2) - (baseDelay / 8),
+      decayTime: baseDelay * 4 * 2 - baseDelay / 8,
       totalReverbTime: baseDelay * 4 * 2, // For 2 bars
     },
     reverbLargeRoom: {
       preDelay: baseDelay / 16,
-      decayTime: (baseDelay * 4 )- (baseDelay / 16),
+      decayTime: baseDelay * 4 - baseDelay / 16,
       totalReverbTime: baseDelay * 4, // For 1 bar
     },
     reverbSmallRoom: {
       preDelay: baseDelay / 32,
-      decayTime: (baseDelay * 2) - (baseDelay / 32),
+      decayTime: baseDelay * 2 - baseDelay / 32,
       totalReverbTime: baseDelay * 2, // For 1/4 note
     },
     reverbTightAmbience: {
       preDelay: baseDelay / 128,
-      decayTime: baseDelay  - (baseDelay / 128),
-      totalReverbTime: baseDelay , // For 1/8 note
+      decayTime: baseDelay - baseDelay / 128,
+      totalReverbTime: baseDelay, // For 1/8 note
     },
   };
 
-return transformValues(rawResults)
-
+  return transformValues(rawResults);
 }
