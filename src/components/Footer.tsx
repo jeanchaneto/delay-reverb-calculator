26128879402809deaf097d57d94050da47974649
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2025-01-12 22:43:55 +0100

    Update footer, compress images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
-
 const Footer = () => {
   return (
     <div className="pb-8 pt-8 max-w-4xl mx-auto absolute bottom-0 right-6 ">
       <p className="text-xs text-foreground-400">
-        &copy; 2023 Created by<a href="https://jean-chane-to.com/" className="text-foreground-300">JCT</a>. All rights reserved. This website uses analytic cookies.
+        &copy; {new Date().getFullYear()} Created by
+        <a href="https://lap-studios.com/" className="text-foreground-300 ml-1">
+          LAP Studios
+        </a>
+        . All rights reserved.
       </p>
     </div>
   );
