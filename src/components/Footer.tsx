baa2f55dbcb2027781106f1acc566b3fc731b8f7
jeanchaneto <jeanchaneto@gmail.com>
2025-11-21 13:58:13 +0100

    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,11 @@ const Footer = () => {
     <div className="pb-8 pt-8 max-w-4xl mx-auto absolute bottom-0 right-6 ">
       <p className="text-xs text-foreground-400">
         &copy; {new Date().getFullYear()} Created by
-        <a href="https://lap-studios.com/" className="text-foreground-300 ml-1">
-          LAP Studios
+        <a
+          href="https://www.instagram.com/lapfr.music/"
+          className="text-foreground-300 ml-1"
+        >
+          LAP (FR)
         </a>
         . All rights reserved.
       </p>
